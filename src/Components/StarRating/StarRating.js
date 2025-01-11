a51b5f77f43f03c8e480051a551f470b51f0d74a
uditrajmr3 <csense2653@gmail.com>
2025-01-11 20:26:14 +0530

    --updated: star component

--- src/Components/StarRating/StarRating.js
+++ src/Components/StarRating/StarRating.js
@@ -1,16 +1,58 @@
+import { useState } from "react";
 import "./StarRating.css";
+import Star from "./Star";
 
-export default function StarRating({ ratingOf = 10 }) {
+export default function StarRating({
+  ratingOf = 10,
+  color = "#ffbc12",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating = () => {},
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
+
+  function updateRating(newRating) {
+    setRating(newRating);
+    onSetRating(newRating);
+  }
+  function updateTempRating(newRating) {
+    setTempRating(newRating);
+  }
+
+  function updateRatingValue() {
+    if (messages.length === ratingOf) {
+      const index = tempRating ? tempRating - 1 : rating - 1;
+      return messages[index];
+    }
+    return tempRating || rating || "";
+  }
+
   const stars = Array.from({ length: ratingOf }, (_, index) => (
-    <span key={index} className="star">
-      ⭐ {index + 1}
-    </span>
+    <Star
+      key={index}
+      color={color}
+      size={size}
+      onClickHandler={() => updateRating(index + 1)}
+      isFilled={tempRating ? tempRating >= index + 1 : rating >= index + 1}
+      onHoverIn={() => updateTempRating(index + 1)}
+      onHoverOut={() => updateTempRating(0)}
+    />
   ));
 
+  const ratingValueStyle = {
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div className="rating">
+    <div className={`rating ${className}`}>
       <div className="stars">{stars}</div>
-      <p className="rating-value">{ratingOf}</p>
+      <p style={ratingValueStyle} className="rating-value">
+        {updateRatingValue()}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./Components/StarRating/StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+const messages = ["Terrible", "Bad", "Okay", "Good", "Amazing"];
+
+function MyRating() {
+  const [rating, setRating] = useState(3);
+
+  return (
+    <div>
+      <StarRating
+        ratingOf={10}
+        color="#1b4acf"
+        defaultRating={rating}
+        onSetRating={setRating}
+      />
+      <p>This movie was rated {rating} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <StarRating />
-    <StarRating ratingOf={5} />
+    <StarRating size={24} />
+    <StarRating size={24} ratingOf={5} messages={messages} />
+    <StarRating color="#fcc419" size={24} />
+    <StarRating ratingOf={20} color="#11a1ff" defaultRating={5} />
+    <MyRating />
   </React.StrictMode>
 );
