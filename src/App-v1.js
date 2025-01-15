e041e409c7203a0556cfb80d249749e98586ac59
uditrajmr3 <csense2653@gmail.com>
2025-01-15 15:31:23 +0530

    --updated: added useEffect to call api --updated: added loader to Listbox

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { tempMovieData } from "./lib/data/temp_movie_data";
 import { tempWatchedData } from "./lib/data/temp_watched_data";
 import NavBar from "./Components/NavBar/NavBar";
@@ -10,24 +10,9 @@ import MovieItem from "./Components/Main/MovieItem";
 import RatingSummary from "./Components/Main/RatingSummary";
 import MovieItemWithRating from "./Components/Main/MovieItemWithRating";
 
-// http://www.omdbapi.com/?i=tt3896198&apikey=a380ef45
-const KEY = "a380ef45";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchMoviesOnMount() {
-      setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=thor`);
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
-    }
-    fetchMoviesOnMount();
-  }, []);
 
   return (
     <React.Fragment>
@@ -40,7 +25,7 @@ export default function App() {
       <Main>
         {/* Left List Block : Shows All Movies List */}
         {/* ListBox component works as a container for MovieList component */}
-        <ListBox isLoading={isLoading}>
+        <ListBox>
           {/* MovieList component takes a function which returns the children */}
           <MovieList
             movies={movies}
--- src/Components/Main/ListBox.js
+++ src/Components/Main/ListBox.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
 
-export default function ListBox({ children }) {
+export default function ListBox({ isLoading, children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="box">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <BuildList isOpen={isOpen} setIsOpen={setIsOpen}>
+          {children}
+        </BuildList>
+      )}
+    </div>
+  );
+}
+
+function BuildList({ isOpen, setIsOpen, children }) {
+  return (
+    <>
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
       {isOpen && children}
-    </div>
+    </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
